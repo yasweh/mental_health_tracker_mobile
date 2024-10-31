b1413620746be5f5b31504f28b1131cd67e92c82
yasweh <myayyas@gmail.com>
2024-10-31 23:22:06 +0700

    Menyelesaikan tutorial 6

---
+++ android/app/src/main/kotlin/com/example/mental_health_tracker_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mental_health_tracker_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mental_health_tracker_mobile/menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.deepPurple,
+        ).copyWith(secondary: Colors.deepPurple[400]),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(), // Assign MyHomePage to the home property
+    );
+  }
+}
