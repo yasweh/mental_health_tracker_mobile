44b16a490c823a943ffdd4961da1141bfa115981
yasweh <myayyas@gmail.com>
2024-11-13 10:18:26 +0700

    tugas 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mental_health_tracker_mobile/menu.dart';
+import 'package:mental_health_tracker_mobile/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,12 +14,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.deepPurple,
-        ).copyWith(secondary: Colors.deepPurple[400]),
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple,).copyWith(secondary: Colors.deepPurple[400]),
         useMaterial3: true,
       ),
-      home: MyHomePage(), // Assign MyHomePage to the home property
+      home: MyHomePage(),
     );
   }
 }
--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker_mobile/widgets/left_drawer.dart';
+import 'package:mental_health_tracker_mobile/widgets/mood_card.dart';
 
-class MyHomePage extends StatelessWidget{
-  MyHomePage({super.key});
-  
-  final String npm = '2306212083'; // NPM
-  final String name = 'Muhammad Yahya Ayyas'; // Nama
-  final String className = 'PBP C'; // Kelas
-  
-      final List<ItemHomepage> items = [
-        ItemHomepage("Lihat Mood", Icons.mood),
-        ItemHomepage("Tambah Mood", Icons.add),
-        ItemHomepage("Logout", Icons.logout),
+class MyHomePage extends StatelessWidget {
+    final String npm = '5000000000'; // NPM
+    final String name = 'Gedagedi Gedagedago'; // Nama
+    final String className = 'PBP S'; // Kelas
+
+    MyHomePage({super.key});
+
+    final List<ItemHomepage> items = [
+      ItemHomepage("Lihat Mood", Icons.mood),
+      ItemHomepage("Tambah Mood", Icons.add),
+      ItemHomepage("Logout", Icons.logout),
     ];
-
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +31,10 @@ class MyHomePage extends StatelessWidget{
         ),
         // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
         backgroundColor: Theme.of(context).colorScheme.primary,
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
+      // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
+      drawer: const LeftDrawer(),
       // Body halaman dengan padding di sekelilingnya.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -125,65 +129,4 @@ class InfoCard extends StatelessWidget {
       ),
     );
   }
-}
-
-class ItemHomepage {
-    final String name;
-    final IconData icon;
-
-    ItemHomepage(this.name, this.icon);
-}
-
-class ItemCard extends StatelessWidget {
-  // Menampilkan kartu dengan ikon dan nama.
-
-  final ItemHomepage item; 
-  
-  const ItemCard(this.item, {super.key}); 
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      // Menentukan warna latar belakang dari tema aplikasi.
-      color: Theme.of(context).colorScheme.secondary,
-      // Membuat sudut kartu melengkung.
-      borderRadius: BorderRadius.circular(12),
-      
-      child: InkWell(
-        // Aksi ketika kartu ditekan.
-        onTap: () {
-          // Menampilkan pesan SnackBar saat kartu ditekan.
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
-            );
-        },
-        // Container untuk menyimpan Icon dan Text
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              // Menyusun ikon dan teks di tengah kartu.
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-  
 }
